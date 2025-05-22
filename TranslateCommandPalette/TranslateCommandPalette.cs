987828141952b8a56aaab86d3ef70ffb927d34cb
matthew1115 <matthew_1115@outlook.com>
2025-05-22 18:36:41 +0800

    Add project files.

---
+++ TranslateCommandPalette/TranslateCommandPalette.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.CommandPalette.Extensions;
+
+namespace TranslateCommandPalette;
+
+[Guid("eeec51db-a156-416a-9bdf-cedfc7fcfdf3")]
+public sealed partial class TranslateCommandPalette : IExtension, IDisposable
+{
+    private readonly ManualResetEvent _extensionDisposedEvent;
+
+    private readonly TranslateCommandPaletteCommandsProvider _provider = new();
+
+    public TranslateCommandPalette(ManualResetEvent extensionDisposedEvent)
+    {
+        this._extensionDisposedEvent = extensionDisposedEvent;
+    }
+
+    public object? GetProvider(ProviderType providerType)
+    {
+        return providerType switch
+        {
+            ProviderType.Commands => _provider,
+            _ => null,
+        };
+    }
+
+    public void Dispose() => this._extensionDisposedEvent.Set();
+}
